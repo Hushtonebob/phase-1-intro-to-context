9125db225f0e9de94b78122845efc6435386fdfd
BobbyDeluna <bobby.deluna2@gmail.com>
2022-06-14 19:40:40 -0700

    commit tree 1af317ed8d2b1d7d0a63cf55ba5928c9191aa73e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+const blam = ["Gray", "Worm", "Security", 1];
+
+function createEmployeeRecord(array){
+    const [firstName, familyName, title] = array;
+    const obj = {firstName, familyName, title, payPerHour, timeInEvents: [], timeOutEvents:[]};
+    return obj 
+};
+
+createEmployeeRecord(blam);
+
+
+function createEmployeeRecords(){
+
+};
